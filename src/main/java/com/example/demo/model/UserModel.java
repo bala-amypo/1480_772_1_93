a6e5e51fa21cd87165fe2544259fda6ad72aedf6
bala-amypo <bala@amypo.in>
2025-12-19 10:18:47 +0000

    User commit

--- src/main/java/com/example/demo/model/UserModel.java
+++ src/main/java/com/example/demo/model/UserModel.java
@@ -1,9 +1,20 @@
 package com.example.demo.model;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class UserModel {
+
+    public enum Role {
+        USER,
+        ADMIN
+    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
