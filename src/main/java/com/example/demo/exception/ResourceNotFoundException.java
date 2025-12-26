d8d82d664e7de72328601e6522170f05f4ead04b
bala-amypo <bala@amypo.in>
2025-12-26 17:49:48 +0000

    User commit

--- src/main/java/com/example/demo/exception/ResourceNotFoundException.java
+++ src/main/java/com/example/demo/exception/ResourceNotFoundException.java
@@ -1,26 +1,10 @@
-// package com.example.demo.exception;
-
-// public class ResourceNotFoundException
-//         extends RuntimeException {
-
-//     public ResourceNotFoundException(String message) {
-//         super(message);
-//     }
-// }
-
-
 package com.example.demo.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
+public class ResourceNotFoundException
+        extends RuntimeException {
 
-/**
- * Custom exception representing “not found” conditions (Requirement 5.1)
- */
-@ResponseStatus(value = HttpStatus.NOT_FOUND)
-public class ResourceNotFoundException extends RuntimeException {
-    
     public ResourceNotFoundException(String message) {
         super(message);
     }
 }
+
