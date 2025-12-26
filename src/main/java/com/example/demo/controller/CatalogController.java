7717712c22a56476eac49a00327e8e2d89e14a75
bala-amypo <bala@amypo.in>
2025-12-26 18:34:48 +0000

    User commit

--- src/main/java/com/example/demo/controller/CatalogController.java
+++ src/main/java/com/example/demo/controller/CatalogController.java
@@ -1,34 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.model.Medication;
-// import com.example.demo.model.ActiveIngredient;
-// import com.example.demo.service.CatalogService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-
-// @RestController
-// @RequestMapping("/api/catalog")
-// public class CatalogController {
-
-//     @Autowired
-//     private CatalogService catalogService;
-
-//     @PostMapping("/ingredients")
-//     public ResponseEntity<ActiveIngredient> addIngredient(@RequestBody ActiveIngredient ingredient) {
-//         return ResponseEntity.ok(catalogService.addIngredient(ingredient));
-//     }
-
-//     @PostMapping("/medications")
-//     public ResponseEntity<Medication> addMedication(@RequestBody Medication medication) {
-//         return ResponseEntity.ok(catalogService.addMedication(medication));
-//     }
-
-//     @GetMapping("/medications/{id}")
-//     public ResponseEntity<Medication> getMedication(@PathVariable Long id) {
-//         return ResponseEntity.ok(catalogService.getMedication(id));
-//     }
-// }
 
 
 package com.example.demo.controller;
--- src/main/java/com/example/demo/controller/InteractionController.java
+++ src/main/java/com/example/demo/controller/InteractionController.java
@@ -1,32 +1,3 @@
-// package com.example.demo.controller;
-
-// import com.example.demo.model.InteractionCheckResult;
-// import com.example.demo.service.InteractionService;
-// import org.springframework.beans.factory.annotation.Autowired;
-// import org.springframework.http.ResponseEntity;
-// import org.springframework.web.bind.annotation.*;
-
-// import java.util.List;
-
-// @RestController
-// @RequestMapping("/api/interactions")
-// public class InteractionController {
-
-//     @Autowired
-//     private InteractionService interactionService;
-
-//     @PostMapping("/check")
-//     public ResponseEntity<InteractionCheckResult> checkInteractions(@RequestBody List<Long> medicationIds) {
-//         InteractionCheckResult result = interactionService.checkInteractions(medicationIds);
-//         return ResponseEntity.ok(result);
-//     }
-
-//     @GetMapping("/{id}")
-//     public ResponseEntity<InteractionCheckResult> getResult(@PathVariable Long id) {
-//         return ResponseEntity.ok(interactionService.getResult(id));
-//     }
-// }
-
 
 
 package com.example.demo.controller;
