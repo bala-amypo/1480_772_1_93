0eec309d8185a2e1bc5e77748bde82ad0bffde15
bala-amypo <bala@amypo.in>
2025-12-26 04:39:31 +0000

    User commit

--- src/main/java/com/example/demo/config/WebConfig.java
+++ src/main/java/com/example/demo/config/WebConfig.java
@@ -7,7 +7,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-public class WebSecurityConfig {
+public class WebConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http)
