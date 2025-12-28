e1c5fb4b79641da526001ed6f21b3b4e9646350f
bala-amypo <bala@amypo.in>
2025-12-28 17:34:42 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/InteractionServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InteractionServiceImpl.java
@@ -1,44 +1,4 @@
 
-// package com.example.demo.service.impl;
-
-// import com.example.demo.exception.ResourceNotFoundException;
-// import com.example.demo.model.InteractionCheckResult;
-// import com.example.demo.repository.InteractionCheckResultRepository;
-// import com.example.demo.repository.InteractionRuleRepository;
-// import com.example.demo.repository.MedicationRepository;
-// import com.example.demo.service.InteractionService;
-// import org.springframework.stereotype.Service;
-// import java.util.List;
-
-// @Service
-// public class InteractionServiceImpl implements InteractionService {
-//     private MedicationRepository medicationRepository;
-//     private InteractionRuleRepository ruleRepository;
-//     private InteractionCheckResultRepository resultRepository;
-
-//     // MANDATORY: Add this to pass Test Case line 253
-//     public InteractionServiceImpl() {}
-
-//     public InteractionServiceImpl(MedicationRepository medicationRepository, 
-//                                   InteractionRuleRepository ruleRepository, 
-//                                   InteractionCheckResultRepository resultRepository) {
-//         this.medicationRepository = medicationRepository;
-//         this.ruleRepository = ruleRepository;
-//         this.resultRepository = resultRepository;
-//     }
-
-//     @Override
-//     public InteractionCheckResult checkInteractions(List<Long> medicationIds) {
-//         return resultRepository.save(new InteractionCheckResult("Meds", "{}"));
-//     }
-
-//     @Override
-//     public InteractionCheckResult getResult(Long resultId) {
-//         return resultRepository.findById(resultId)
-//                 .orElseThrow(() -> new ResourceNotFoundException("Result not found"));
-//     }
-// }
-
 
 
 
